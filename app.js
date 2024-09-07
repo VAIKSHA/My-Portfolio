dcf56d7eac4620cbee110dc29eca1caa7184a108
Vishal Sharma <vs6143214@gmail.com>
2024-09-07 22:14:25 +0530

    glow effect

---
+++ app.js
@@ -0,0 +1,9 @@
+// Animation Start
+var typed = new Typed('#element', {
+    strings: ['<b>Web Developer !</b> ', '<b>Web Designer !</b> ', '<b>Video Editor !</b> ','<b>Content Creator ! </b>','<b>Youtuber !</b> '],
+    typeSpeed: 100,
+  });
+// onclick Event Nav Buttons
+// function toggleActive(element) {
+//     element.classList.toggle('active-nav-btn');
+// }
